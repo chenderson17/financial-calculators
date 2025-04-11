d53360b6ccf7953b1cc005bd73574e7c39a3f1cf
c h <carolinehen223@gmail.com>
2025-04-11 11:28:22 -0700

    added input to calculator

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,26 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         // example: System.out.printf("Total interest: $%.2f", mortgage_calculator(53000,7.625,15));
-        System.out.printf("Enter the principal: ");
+        System.out.printf("Enter the principal:");
         double principal = scanner.nextDouble();
+        System.out.println("Enter the interest rate: ");
+        double interest_rate = scanner.nextDouble();
+        System.out.println("Enter the loan length(years): ");
+        double loan_length = scanner.nextDouble();
+        mortgage_calculator(principal,interest_rate,loan_length);
+
+
     }
 
 
     //Mortgage Calculator
     //accept the principal, interest rate, and loan length
-    public static double mortgage_calculator(double principal, double interest_rate,double loan_length){
+    public static void mortgage_calculator(double principal, double interest_rate,double loan_length){
         double n = loan_length * 12; // number of monthly payments
         double i = (interest_rate/100)/12;
         double m = principal * (i * (Math.pow(1 + i, n)) / (Math.pow(1+i,n) -1));
-        System.out.println(m);
-        return  (m * n) - principal;
+        double total_interest = (m*n) - principal;
+        System.out.printf("Monthly Payments:$%.2f\n",m);
+        System.out.printf("Total Interest:$%.2f",total_interest);
     }
 }
