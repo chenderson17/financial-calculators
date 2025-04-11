29b52133b6d075e3d63dddea56dd0dbfe5901b6f
c h <carolinehen223@gmail.com>
2025-04-11 13:46:23 -0700

    added scanner functions for both calculators

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,37 @@ import java.util.Scanner;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        // example: System.out.printf("Total interest: $%.2f", mortgage_calculator(53000,7.625,15));
-        System.out.printf("Enter the principal:");
-        double principal = scanner.nextDouble();
-        System.out.println("Enter the interest rate: ");
-        double interest_rate = scanner.nextDouble();
-        System.out.println("Enter the loan length(years): ");
-        double loan_length = scanner.nextDouble();
-        mortgage_calculator(principal,interest_rate,loan_length);
+        System.out.print("Type 'm' for mortgage calcualtor or 'f' for future value: ");
+        String input = scanner.next();
+        if(input.toLowerCase().equals("m")){
+            //calculate mortgage
+            System.out.print("Enter the principal:");
+            double principal = scanner.nextDouble();
+            System.out.print("Enter the interest rate: ");
+            double interest_rate = scanner.nextDouble();
+            System.out.print("Enter the loan length(years): ");
+            double loan_length = scanner.nextDouble();
+            mortgage_calculator(principal,interest_rate,loan_length);
+        }
+        else if(input.toLowerCase().equals(("f"))){
+            //calcualte cd future
+            System.out.print("Enter the deposit: ");
+            double deposit = scanner.nextDouble();
+            System.out.print("Enter the interest rate: ");
+            double interest_rate = scanner.nextDouble();
+            System.out.print("Enter the number of years: ");
+            double years = scanner.nextDouble();
+            future_value(deposit,interest_rate,years);
+        }
+        else{
 
+        }
 
     }
 
 
-    //Mortgage Calculator
+
+    // Calulator 1: Mortgage Calculator
     //accept the principal, interest rate, and loan length
     public static void mortgage_calculator(double principal, double interest_rate,double loan_length){
         double n = loan_length * 12; // number of monthly payments
@@ -27,4 +44,21 @@ public class Main {
         System.out.printf("Monthly Payments:$%.2f\n",m);
         System.out.printf("Total Interest:$%.2f",total_interest);
     }
+    // Calculator 2: Future Value
+    /* a. It would accept the deposit, interest rate, and number of
+          years from the user
+        b. It would display the future value and the total interest
+            earned
+
+     */
+    public static void future_value(double deposit, double interest_rate, double years){
+        //FV = P × (1 + (r / 365))^(365 × t)
+        double r = interest_rate/100.0;
+        double fv = deposit * Math.pow((1 + (r/365.0)), (365.0 * years));
+        double total_interest = fv - deposit;
+        System.out.printf("Total Earned:$%.2f\n",fv);
+        System.out.printf("Total Interest:$%.2f\n",total_interest);
+
+    }
+
 }
